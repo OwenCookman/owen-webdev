c81c1038c1160f0ae3881e6660bab02e5fe968d1
OwenCookman <ozzycookman@hotmail.com>
2020-03-30 19:57:27 +0000

    Added login and logout authorisation

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 
 # Create your views here.
@@ -10,14 +11,29 @@ def index(request):
     return render(request, 'index.html')
 
 
+@login_required
 def logout(request):
-    """Logs the user out"""
+    """Logs the user out if they are logged in"""
     auth.logout(request)
     messages.success(request, "You have been logged out successfully")
     return redirect(reverse('index'))
 
 
 def Login(request):
-    """Returns the login page"""
-    login_form = UserLoginForm()
+    """Returns the login page, if user is logged in this returns index page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(
+                    request, "You have been successfully logged in")
+                else:
+                    login_form.add_error(
+                        None, "Your Username and/or Password is incorrect")
+
     return render(request, 'login.html', {"login_form": login_form})
