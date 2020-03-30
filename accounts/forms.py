32df2cace51f26b085d81f809d449a715d98481f
OwenCookman <ozzycookman@hotmail.com>
2020-03-30 20:56:42 +0000

    Form validation implemented for Registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -16,6 +16,24 @@ class UserRegistrationForm(UserCreationForm):
     password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
     password2 = forms.CharField(
         label="Confirm Password", widget=forms.PasswordInput)
-        class Meta:
-            model=User
-            fields=['email', 'username', 'password1', 'password2']
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if User.object.filter(email=email).exclude(username=username):
+            raise forms.ValidationError(
+                u'That Email address is already in use')
+        return email
+    
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if not password1 or not password2:
+            raise ValidationError("Please confirm your password")
+        if password1 != password2:
+            raise ValidationError("Those passwords do not match")
+        return password2
