1e9591ee28632ff09ffba298bd79986340916e39
OwenCookman <ozzycookman@hotmail.com>
2020-03-30 17:08:37 +0000

    Added login and logout views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
 
 # Create your views here.
+
+def index(request):
+    """Returns the index.html file"""
+    return render(request, 'index.html')
+
+def logout(request):
+    """Logs the user out"""
+    auth.logout(request)
+    messages.success(request, "You have been logged out successfully")
+    return redirect(reverse('index'))
+
+def Login(request):
+    """Returns the login page"""
+    return render(request, 'login.html')
+
+
