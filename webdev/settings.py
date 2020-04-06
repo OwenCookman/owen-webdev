bf1fe544dd2d11130696676ebb6e767b9f7fb746
OwenCookman <ozzycookman@hotmail.com>
2020-04-06 15:36:21 +0000

    Added Bootswatch theme

--- webdev/settings.py
+++ webdev/settings.py
@@ -76,17 +76,17 @@ WSGI_APPLICATION = 'webdev.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if "DATABASE_URL" in os.environ:
-    DATABASE = {'default': dj_database_url.parse(
-        os.environ.get('DATABASE_URL'))}
-else:
-    print("Database URL not found, using SQLite instead")
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+# if "DATABASE_URL" in os.environ:
+#    DATABASE = {'default': dj_database_url.parse(
+#        os.environ.get('DATABASE_URL'))}
+# else:
+#    print("Database URL not found, using SQLite instead")
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 
 # Password validation
