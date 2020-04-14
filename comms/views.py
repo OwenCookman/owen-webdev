314f56e55f179b8afa9efedeb1602380594d859b
OwenCookman <ozzycookman@hotmail.com>
2020-04-14 09:53:05 +0000

    Created contact view and imported ContactForm from forms

--- comms/views.py
+++ comms/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from comms.forms import ContactForm
 
 # Create your views here.
+
+def contact(request):
+    return render(request, 'contact.html')
