102eeb80e322b73e50537dd58130a26a25b503df
OwenCookman <ozzycookman@hotmail.com>
2020-04-14 12:09:20 +0000

    Created portfolio app

--- webdev/settings.py
+++ webdev/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['owen-cookman-web-dev.herokuapp.com']
+ALLOWED_HOSTS = ['owen-cookman-web-dev.herokuapp.com', 'localhost']
 
 
 # Application definition
