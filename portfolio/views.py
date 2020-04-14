898af0571783c88b95848b968011771f1201e120
OwenCookman <ozzycookman@hotmail.com>
2020-04-14 12:19:37 +0000

    Created portfolio view

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+def portfolio(request):
+    """Returns the portfolio.html file"""
+    return render(request, 'portfolio.html')
