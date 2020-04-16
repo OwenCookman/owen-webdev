89a14519049702bcb931e0ae1c861196894c9ab6
OwenCookman <ozzycookman@hotmail.com>
2020-04-16 13:44:59 +0000

    Commented out S3 section so styles can be viewed locally

--- webdev/settings.py
+++ webdev/settings.py
@@ -130,20 +130,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-AWS_S3_OBJECT_PARAMETERS = {
-    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-    'CacheControl': 'max-age=94608000'
-}
 
-AWS_STORAGE_BUCKET_NAME = 'webdevbucket'
-AWS_S3_REGION_NAME = 'eu-west-2'
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+# Deployment settings for static
+#AWS_S3_OBJECT_PARAMETERS = {
+#    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+#    'CacheControl': 'max-age=94608000'
+#}
 
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+#AWS_STORAGE_BUCKET_NAME = 'webdevbucket'
+#AWS_S3_REGION_NAME = 'eu-west-2'
+#AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+#AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
-STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+#AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+#STATICFILES_LOCATION = 'static'
+#STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
 
@@ -155,5 +157,7 @@ STATICFILES_DIRS = [
 
 STRIPE_PUBLISHABLE = os.environ.get("STRIPE_PUBLISHABLE")
 STRIPE_SECRET = os.environ.get("STRIPE_SECRET")
+
+# Page messages
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
