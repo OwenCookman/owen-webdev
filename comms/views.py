9071211f7b8a5a16648dc68717a8df76c0ed6960
OwenCookman <ozzycookman@hotmail.com>
2020-04-18 18:29:04 +0000

    Added form submission to database for contact form

--- comms/views.py
+++ comms/views.py
@@ -9,6 +9,19 @@ from comms.forms import ContactForm
 def contact(request):
     if request.method == "POST":
         contact_form = ContactForm(request.POST)
+
+         if contact_form.is_valid():
+            contact_form.save()
+
+            work = auth.authenticate(website=request.POST['website'],
+                                     functionality=request.POST['functionality'],
+                                     url=request.POST['url'],
+                                     business=request.POST['business'],
+                                     customer=request.POST['customer'],
+                                     message=request.POST['message'])
+            else:
+                messages.error(
+                    request, "Sorry, your request could not be submitted, please try again")
     else:
         contact_form = ContactForm()
     return render(request, 'contact.html', {'contact_form': contact_form})
