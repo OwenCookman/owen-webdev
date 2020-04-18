518ee3f45fa3d0b365b0eb0f88d423f134a16031
OwenCookman <ozzycookman@hotmail.com>
2020-04-18 18:35:30 +0000

    Removed labels and widgets from query model

--- comms/models.py
+++ comms/models.py
@@ -5,18 +5,11 @@ from django.contrib.auth.models import User
 
 
 class query(models.Model):
-    website = models.CharField(
-        max_length=200, label="What kind of website are you looking for?*")
-    functionality = models.CharField(
-        max_length=200, label="What functionality would you like on your website?*")
-    url = models.CharField(
-        max_length=200, label="pre existing websites URL", required="False")
-    designs = models.FileInput()
-    business = models.CharField(max_length=200,
-                                widget=Textarea, label="What does your business do?")
-    customer = models.CharField(max_length=200,
-                                widget=Textarea, label="Who are your clients/customers?")
-    message = models.CharField(max_length=200,
-                               widget=Textarea, label="Please enter a message here, give as much information as possible that you think may be useful")
+    website = models.CharField(max_length=200)
+    functionality = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    business = models.CharField(max_length=200)
+    customer = models.CharField(max_length=200)
+    message = models.CharField(max_length=200)
 
     user = models.ForeignKey(User)
