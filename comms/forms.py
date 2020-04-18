a78b34f2d31e7e93f029510575fd60bc224a465e
OwenCookman <ozzycookman@hotmail.com>
2020-04-18 18:26:55 +0000

    Reworked ContactForm as a ModelForm

--- comms/forms.py
+++ comms/forms.py
@@ -1,16 +1,9 @@
 from django import forms
+from .models import query
 
 
-class ContactForm(forms.Form):
-    """The form users use to give information on the website
-    they would like to have built"""
-    website = forms.CharField(label="What kind of website are you looking for?*")
-    functionality = forms.CharField(label="What functionality would you like on your website?*")
-    url = forms.CharField(label="pre existing websites URL", required="False")
-    designs = forms.FileInput()
-    business = forms.CharField(
-        widget=forms.Textarea, label="What does your business do?")
-    customer = forms.CharField(
-        widget=forms.Textarea, label="Who are your clients/customers?")
-    message = forms.CharField(
-        widget=forms.Textarea, label="Please enter a message here, give as much information as possible that you think may be useful")
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = query
+        fields = (website, functionality, url, business, customer, message)
+    
