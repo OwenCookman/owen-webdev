dbb7c864d89b6992241dc437339c96d739f4b3e5
OwenCookman <ozzycookman@hotmail.com>
2020-04-18 18:50:45 +0000

    Imported auth and messages

--- comms/views.py
+++ comms/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
 from comms.forms import ContactForm
 
 # Create your views here.
@@ -10,18 +11,18 @@ def contact(request):
     if request.method == "POST":
         contact_form = ContactForm(request.POST)
 
-         if contact_form.is_valid():
+        if contact_form.is_valid():
             contact_form.save()
 
-            work = auth.authenticate(website=request.POST['website'],
-                                     functionality=request.POST['functionality'],
-                                     url=request.POST['url'],
-                                     business=request.POST['business'],
-                                     customer=request.POST['customer'],
-                                     message=request.POST['message'])
-            else:
-                messages.error(
-                    request, "Sorry, your request could not be submitted, please try again")
+            query = auth.authenticate(website=request.POST['website'],
+                                      functionality=request.POST['functionality'],
+                                      url=request.POST['url'],
+                                      business=request.POST['business'],
+                                      customer=request.POST['customer'],
+                                      message=request.POST['message'])
+        else:
+            messages.error(
+                request, "Sorry, your request could not be submitted, please try again")
     else:
         contact_form = ContactForm()
     return render(request, 'contact.html', {'contact_form': contact_form})
