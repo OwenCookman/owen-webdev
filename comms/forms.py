1fe8f0029d67763e1d2b5150d73a24c9e7c28302
OwenCookman <ozzycookman@hotmail.com>
2020-04-18 18:49:35 +0000

    Made fields a list in ContactForm

--- comms/forms.py
+++ comms/forms.py
@@ -5,5 +5,5 @@ from .models import query
 class ContactForm(forms.ModelForm):
     class Meta:
         model = query
-        fields = (website, functionality, url, business, customer, message)
+        fields = ['website', 'functionality', 'url', 'business', 'customer', 'message']
     
