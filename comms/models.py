f92860559708bdb8cdb7f3c01143258552042834
OwenCookman <ozzycookman@hotmail.com>
2020-04-19 13:11:53 +0000

    Replaced input for website and functionality with choices in order model

--- comms/models.py
+++ comms/models.py
@@ -5,8 +5,22 @@ from django.contrib.auth.models import User
 
 
 class order(models.Model):
-    website = models.CharField(max_length=200)
-    functionality = models.CharField(max_length=200)
+    WEBSITE_CHOICES = (
+        ('Landing page/Brochure website', 'Landing page/Brochure website'),
+        ('Multipaged information website',
+         'Multipaged information website'),
+        ('Ecommerce/Webstore', 'Ecommerce/Webstore'),
+        ('Something Else', 'Something Else')
+    )
+    FUNCTIONALITY_CHOICES = (
+        ('Online Payments', 'Online Payments'),
+        ('Shopping Cart/Checkout', 'Shopping Cart/Checkout'),
+        ('User Registration & Login', 'User Registration & Login'),
+        ('Something Else', 'Something Else')
+    )
+    website = models.CharField(max_length=50, choices=WEBSITE_CHOICES)
+    functionality = models.CharField(
+        max_length=50, choices=FUNCTIONALITY_CHOICES)
     url = models.CharField(max_length=200)
     business = models.CharField(max_length=200)
     customer = models.CharField(max_length=200)
