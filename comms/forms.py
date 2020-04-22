4c1084b700be88bfeddea39b321e09d6870306ea
OwenCookman <ozzycookman@hotmail.com>
2020-04-22 16:14:25 +0000

    Changed models import

--- comms/forms.py
+++ comms/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from .models import order
+from . import models
 
 
 class ContactForm(forms.ModelForm):
     class Meta:
-        model = order
-        exclude = ('client',)
+        model = models.order
+        exclude = ('client', 'date')
         widgets = {
             'website': forms.RadioSelect(),
             'functionality': forms.CheckboxSelectMultiple(),
