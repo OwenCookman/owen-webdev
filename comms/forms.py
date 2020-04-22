fe4a67bb82465dcc0623fdc25a408497ad67d2b0
OwenCookman <ozzycookman@hotmail.com>
2020-04-22 15:31:50 +0000

    Reworked ContactForm to include widgets and exclude client

--- comms/forms.py
+++ comms/forms.py
@@ -5,4 +5,11 @@ from .models import order
 class ContactForm(forms.ModelForm):
     class Meta:
         model = order
-        fields = '__all__'
+        exclude = ('client',)
+        widgets = {
+            'website': forms.RadioSelect(),
+            'functionality': forms.CheckboxSelectMultiple(),
+            'business_type': forms.Textarea(),
+            'customer': forms.Textarea(),
+            'message': forms.Textarea(),
+        }
