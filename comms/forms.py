af1e3c02ddde61ed39783bd923dd366837d04e4d
OwenCookman <ozzycookman@hotmail.com>
2020-04-22 18:08:12 +0000

    Set fields to include instead of exclude

--- comms/forms.py
+++ comms/forms.py
@@ -5,7 +5,9 @@ from . import models
 class ContactForm(forms.ModelForm):
     class Meta:
         model = models.order
-        exclude = ('client', 'date')
+        fields = [
+            'business_name', 'website', 'functionality', 'url', 'business_type', 'customer', 'message'
+        ]
         widgets = {
             'website': forms.RadioSelect(),
             'functionality': forms.CheckboxSelectMultiple(),
