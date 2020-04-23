a8e79e9de7768bad1f1693fed88bb4f1c32588ac
OwenCookman <ozzycookman@hotmail.com>
2020-04-23 15:07:05 +0000

    Commented out functionality choices and made functionality a text area

--- comms/models.py
+++ comms/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
@@ -12,18 +13,18 @@ class order(models.Model):
         ('Ecommerce/Webstore', 'Ecommerce/Webstore'),
         ('Something Else', 'Something Else')
     )
-    FUNCTIONALITY_CHOICES = (
-        ('Online Payments', 'Online Payments'),
-        ('Shopping Cart/Checkout', 'Shopping Cart/Checkout'),
-        ('User Registration & Login', 'User Registration & Login'),
-        ('Something Else', 'Something Else')
-    )
+    # FUNCTIONALITY_CHOICES = (
+    #     ('Online Payments', 'Online Payments'),
+    #     ('Shopping Cart/Checkout', 'Shopping Cart/Checkout'),
+    #     ('User Registration & Login', 'User Registration & Login'),
+    #     ('Something Else', 'Something Else')
+    # )
     business_name = models.CharField(
         "What is the name of your business?", max_length=50)
     website = models.CharField(
         "What kind of website would you like?", max_length=50, choices=WEBSITE_CHOICES)
     functionality = models.CharField(
-        "What functionality would you like your website to have?", max_length=50, choices=FUNCTIONALITY_CHOICES)
+        "What functionality would you like your website to have?", max_length=200)
     url = models.CharField(
         "If you already have a website please add the URL here", max_length=200)
     business_type = models.CharField(
@@ -32,8 +33,7 @@ class order(models.Model):
         "Who are your main customers/clients?", max_length=200)
     message = models.CharField(
         "Please provide any extra information that you think might be useful", max_length=200)
-    client = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
+    client = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, default=1)
 
     def __str__(self):
         return self.business_name
