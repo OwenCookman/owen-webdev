aabf486c8b42a4d3b2c070653b44b8b40b7e704b
OwenCookman <ozzycookman@hotmail.com>
2020-04-23 12:25:51 +0000

    Brought back cleaned_data

--- comms/views.py
+++ comms/views.py
@@ -12,6 +12,16 @@ def contact(request):
         contact_form = forms.ContactForm(request.POST)
 
         if contact_form.is_valid():
+            print("is valid")
+            contact_form.cleaned_data(
+                business_name=request.POST['business_name'])
+            contact_form.cleaned_data(website=request.POST['website'])
+            contact_form.cleaned_data(functionality=request.POST['functionality'])
+            contact_form.cleaned_data(url=request.POST['url'])
+            contact_form.cleaned_data(business_type=request.POST['business_type'])
+            contact_form.cleaned_data(customer=request.POST['customer'])
+            contact_form.cleaned_data(message=request.POST['message'])
+            contact_form.cleaned_data(date=request.POST['date'])
             contact = contact_form.save(commit=False)
             contact.client = request.User
             contact.save()
@@ -19,5 +29,6 @@ def contact(request):
             messages.error(
                 request, "Sorry, your request could not be submitted, please try again")
     else:
+        print("failed")
         contact_form = forms.ContactForm()
     return render(request, 'contact.html', {'contact_form': contact_form})
