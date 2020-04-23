4dc9704311ddf500ed4e566b79b1aa647777ae86
OwenCookman <ozzycookman@hotmail.com>
2020-04-23 12:25:28 +0000

    Neatened up order model

--- comms/models.py
+++ comms/models.py
@@ -18,14 +18,16 @@ class order(models.Model):
         ('User Registration & Login', 'User Registration & Login'),
         ('Something Else', 'Something Else')
     )
-    business_name = models.CharField("What is the name of your business?", max_length=50)
+    business_name = models.CharField(
+        "What is the name of your business?", max_length=50)
     website = models.CharField(
         "What kind of website would you like?", max_length=50, choices=WEBSITE_CHOICES)
     functionality = models.CharField(
         "What functionality would you like your website to have?", max_length=50, choices=FUNCTIONALITY_CHOICES)
     url = models.CharField(
         "If you already have a website please add the URL here", max_length=200)
-    business_type = models.CharField("What does your business do?", max_length=200)
+    business_type = models.CharField(
+        "What does your business do?", max_length=200)
     customer = models.CharField(
         "Who are your main customers/clients?", max_length=200)
     message = models.CharField(
