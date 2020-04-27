ca59101fcae2770e951c0e97eb648a40d3ba2c4f
OwenCookman <ozzycookman@hotmail.com>
2020-04-27 14:00:27 +0000

    Gave order model fields appropriate max lengths

--- comms/models.py
+++ comms/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
@@ -18,15 +17,15 @@ class order(models.Model):
     website = models.CharField(
         "What kind of website would you like?", max_length=50, choices=WEBSITE_CHOICES)
     functionality = models.CharField(
-        "What would you like users to do on your website?", max_length=200)
+        "What would you like users to do on your website?", max_length=1000)
     url = models.CharField(
         "If you already have a website please add the URL here", max_length=200)
     business_type = models.CharField(
-        "What does your business do?", max_length=200)
+        "What does your business do?", max_length=2000)
     customer = models.CharField(
-        "Who are your main customers/clients?", max_length=200)
+        "Who are your main customers/clients?", max_length=2000)
     message = models.CharField(
-        "Please provide any extra information that you think might be useful", max_length=200)
+        "Please provide any extra information that you think might be useful", max_length=3000)
     client = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, default=1)
     date = models.DateTimeField(auto_now_add=True)
     price = models.FloatField(default=0)
