2315b9e13d062d9f93998c198c7eecbd4207747e
OwenCookman <ozzycookman@hotmail.com>
2020-04-28 15:24:26 +0000

    Removed unnecessary accounts from views import

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accounts.views import registration, login, logout, user_profile
+from .views import registration, login, logout, user_profile
 
 urlpatterns = [
     path('logout/', logout, name="logout"),
