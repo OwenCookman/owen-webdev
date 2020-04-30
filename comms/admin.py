f8b09d3c929cbacadea902cb9605abaa240abbd9
OwenCookman <ozzycookman@hotmail.com>
2020-04-30 14:46:25 +0000

    Added question to admin panel

--- comms/admin.py
+++ comms/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import order
+from .models import order, question
 
 # Register your models here.
 
 admin.site.register(order)
+admin.site.register(question)
