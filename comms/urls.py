38b8e653ef572ec816385b6b302964a8a030e69d
OwenCookman <ozzycookman@hotmail.com>
2020-05-01 14:42:54 +0000

    Imported EditQuestion and made URL for it

--- comms/urls.py
+++ comms/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from comms.views import contact, question
+from comms.views import contact, question, EditQuestion
 
 urlpatterns = [
     path('contact/', contact, name="contact"),
     path('question/', question, name="question"),
+    path('question/<slug>', EditQuestion, name="edit_question"),
 ]
