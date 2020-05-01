5308f5dc6234afb14bbd5f4ffabf6f0d23501922
OwenCookman <ozzycookman@hotmail.com>
2020-05-01 14:43:43 +0000

    Question form saves without authentication

--- comms/views.py
+++ comms/views.py
@@ -15,7 +15,13 @@ def question(request):
         question_form = forms.QuestionForm(request.POST)
 
         if question_form.is_valid():
-            question_form.save()
+            question = question_form.save(commit=False)
+            if request.user.is_authenticated:
+                question.client = request.user
+                question.save()
+            else:
+                question.client = None
+                question.save()
             messages.success(request, "Thank you for your message, I will get back to you shortly")
             return redirect('index')
 
@@ -24,7 +30,20 @@ def question(request):
 
     else:
         question_form = forms.QuestionForm()
-    return render(request, question.html, {"question_form": question_form})
+    return render(request, 'question.html', {"question_form": question_form})
+
+
+def EditQuestion(request, slug):
+    data = question.objects.get(id=slug)
+    question_form = forms.QuestionForm(instance=data)
+
+    if request.method == "POST":
+        question_form = forms.QuestionForm(request.POST, instance=data)
+
+        if question_form.is_valid():
+            question_form.save()
+
+    return render(request, 'question.html', {"question_form": question_form})
 
 
 @login_required
