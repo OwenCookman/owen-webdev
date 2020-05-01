3fd417a4f6190f5bb5044f3f9e43b8d3523649fd
OwenCookman <ozzycookman@hotmail.com>
2020-05-01 11:04:13 +0000

    Created URL for question

--- comms/urls.py
+++ comms/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from comms.views import contact
+from comms.views import contact, question
 
 urlpatterns = [
-    path('contact/', contact, name="contact")
+    path('contact/', contact, name="contact"),
+    path('question/', question, name="question"),
 ]
