cdde3ef9f14ae6f0536ab3efd89462c58e0ad8f9
OwenCookman <ozzycookman@hotmail.com>
2020-05-04 20:05:47 +0000

    Imported delete_question and created path

--- comms/urls.py
+++ comms/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import contact, query, edit_question
+from .views import contact, create_question, edit_question, delete_question
 
 urlpatterns = [
     path('contact/', contact, name="contact"),
-    path('query/', query, name="question"),
+    path('query/', create_question, name="question"),
     path('edit_question/<slug>', edit_question, name="edit_question"),
+    path('delete_question/<slug>', delete_question, name="delete_question"),
 ]
