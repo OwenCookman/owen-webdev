0fb2ca955bbfa8ab77b1b3963dcd2882e5f050f0
OwenCookman <ozzycookman@hotmail.com>
2020-05-04 21:48:50 +0000

    credit_card_number changed to lower case

--- payments/forms.py
+++ payments/forms.py
@@ -5,7 +5,7 @@ class MakePayment(forms.Form):
     MONTH_CHOICES = [(i, i) for i in range(1, 12)]
     YEAR_CHOICES = [(i, i) for i in range(2020, 2050)]
 
-    CREDIT_CARD_NUMBER = forms.CharField(label="Card Number", required=False)
+    credit_card_number = forms.CharField(label="Card Number", required=False)
     CVV = forms.CharField(label="Security Code (CVV)", required=False)
     expiry_month = forms.ChoiceField(label="Month", choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(label="Year", choices=YEAR_CHOICES, required=False)
