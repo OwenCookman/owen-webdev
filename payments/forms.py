8ab7d69b22836695aad020fbd353a32ee21dfc69
OwenCookman <ozzycookman@hotmail.com>
2020-05-04 12:11:53 +0000

    Changed year choice to this year

--- payments/forms.py
+++ payments/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class MakePayment(forms.Form):
     MONTH_CHOICES = [(i, i) for i in range(1, 12)]
-    YEAR_CHOICES = [(i, i) for i in range(2000, 2050)]
+    YEAR_CHOICES = [(i, i) for i in range(2020, 2050)]
 
     CREDIT_CARD_NUMBER = forms.CharField(label="Card Number", required=False)
     CVV = forms.CharField(label="Security Code (CVV)", required=False)
