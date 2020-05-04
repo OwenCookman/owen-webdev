d01b7a35ce554fc587be1a01b282d227a9c5033c
OwenCookman <ozzycookman@hotmail.com>
2020-05-04 12:06:07 +0000

    Made extra info not required in order model

--- comms/models.py
+++ comms/models.py
@@ -13,19 +13,19 @@ class order(models.Model):
         ('Something Else', 'Something Else')
     )
     business_name = models.CharField(
-        "What is the name of your business?", max_length=50)
+        "What is the name of your business?*", max_length=50)
     website = models.CharField(
-        "What kind of website would you like?", max_length=50, choices=WEBSITE_CHOICES)
+        "What kind of website would you like?*", max_length=50, choices=WEBSITE_CHOICES)
     functionality = models.CharField(
-        "What would you like users to do on your website?", max_length=1000)
+        "What would you like users to do on your website?*", max_length=1000)
     url = models.CharField(
         "If you already have a website please add the URL here", max_length=200, null=True, blank=True)
     business_type = models.CharField(
-        "What does your business do?", max_length=2000)
+        "What does your business do?*", max_length=2000)
     customer = models.CharField(
-        "Who are your main customers/clients?", max_length=2000)
+        "Who are your main customers/clients?*", max_length=2000)
     message = models.CharField(
-        "Please provide any extra information that you think might be useful", max_length=3000)
+        "Please provide any extra information that you think might be useful", max_length=3000, null=True, blank=True)
     client = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, default=1)
     date = models.DateTimeField(auto_now_add=True)
     price = models.FloatField(default=0)
