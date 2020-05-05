d6374aab9ff2401dd5fcaaeb9df0a555de2c9e75
OwenCookman <ozzycookman@hotmail.com>
2020-05-05 17:05:26 +0000

    Added on click functions for modals

--- static/js/script.js
+++ static/js/script.js
@@ -4,5 +4,21 @@ $(document).ready(function () {
         window.print();
     });
 
+    $('#TailoredWebsite').click(function() {
+        $('#TailoredWebsiteModal').modal('show');
+    });
+
+    $('#TargetedDesign').click(function() {
+        $('#TargetedDesignModal').modal('show');
+    });
+
+    $('#ModernLanguages').click(function() {
+        $('#ModernLanguagesModal').modal('show');
+    });
+
+    $('#Responsive').click(function() {
+        $('#ResponsiveModal').modal('show');
+    });
+
 
 });
