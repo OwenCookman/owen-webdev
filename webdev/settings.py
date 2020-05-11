43a885772dda968ca324aa4dd736a8062433529b
OwenCookman <ozzycookman@hotmail.com>
2020-05-11 08:41:22 +0000

    DEBUG set to False

--- webdev/settings.py
+++ webdev/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 
 from os import path
 if path.exists("env.py"):
-    import env
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['owen-cookman-web-dev.herokuapp.com', 'localhost']
 
