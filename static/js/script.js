c3fd0e922b571b94efeaf72d9030b7d85e0f94b6
OwenCookman <ozzycookman@hotmail.com>
2020-05-11 16:09:07 +0000

    Added comments to JS file

--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,36 @@
 $(document).ready(function () {
 
-    $('#PrintPage').click(function() {
+    // Print page function
+    $('#PrintPage').click(function () {
         window.print();
     });
 
-    $('#TailoredWebsite').click(function() {
+    // Click function for Tailored Website modal
+    $('#TailoredWebsite').click(function () {
         $('#TailoredWebsiteModal').modal('show');
     });
 
-    $('#TargetedDesign').click(function() {
+    // Click function for Targeted design modal
+    $('#TargetedDesign').click(function () {
         $('#TargetedDesignModal').modal('show');
     });
 
-    $('#ModernLanguages').click(function() {
+    // Click function for Modern languages modal
+    $('#ModernLanguages').click(function () {
         $('#ModernLanguagesModal').modal('show');
     });
 
-    $('#Responsive').click(function() {
+    // Click function for Responsive modal
+    $('#Responsive').click(function () {
         $('#ResponsiveModal').modal('show');
     });
 
-    $('.rotateClick1').click(function() {
+    // Click functions to toggle animation classes
+    $('.rotateClick1').click(function () {
         $('.rotate1').toggleClass('down');
     });
 
-    $('.rotateClick2').click(function() {
+    $('.rotateClick2').click(function () {
         $('.rotate2').toggleClass('down');
     });
 
